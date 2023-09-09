912e48a4275f6d59b8cc0346491ab8da6614c531
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2023-09-09 16:56:03 +0100

    Added a list of pokemon with a tracker for the largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
 //array containing a list of Pokemon
 let pokemonList = [
-  { name: "Bulbasaur", weight: 6.9, type: ["grass", "poison"] },
-  { name: "Ivysaur", weight: 13, type: ["grass", "poison"] },
-  { name: "Venasaur", weight: 100, type: ["grass", "poison"] },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Ivysaur",
+    weight: 13,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Venasaur",
+    weight: 100,
+    type: ["grass", "poison"],
+  },
 ];
 
-console.log(pokemonList)
+let pokeList = document.querySelector("#pokelist"); //saving the pokemon list HTML element in a variable
+let largestWeight = 0; //variable to track the largest weight of a pokemon
+let largestPokemon; //variable to track the largest pokemon
+
+//for loop to add each pokemon as a list item to the pokemon list
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokeItem = document.createElement("li");
+  let text = document.createTextNode(
+    `${pokemonList[i].name} (weight: ${pokemonList[i].weight})`
+  );
+
+  //updating the largest pokemon variables with each loop iteration
+  if (pokemonList[i].weight > largestWeight) {
+    largestWeight = pokemonList[i].weight;
+    largestPokemon = pokemonList[i];
+  }
+
+  //appending the pokemon to the list
+  pokeItem.setAttribute("id", pokemonList[i].name);
+  pokeItem.appendChild(text);
+  pokeList.appendChild(pokeItem);
+}
+
+//adding a note to the largest pokemon
+document.querySelector(`#${largestPokemon.name}`).appendChild(document.createTextNode(" - That's a chunky boi!!"))
