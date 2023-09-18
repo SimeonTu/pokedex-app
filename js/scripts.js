65538ae2244f827366bdfb1d52dae13bea2b0a7a
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2023-09-18 16:11:42 +0100

    Load Pokemon from pokeAPI
    -Clicking on a pokemon name also displays more info about them in the console.
    -Added polyfills for promises and fetch functions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,36 @@ let pokemonRepository = (function () {
   let pokeList = document.querySelector("#pokelist"); //saving the pokemon list HTML element in a variable
   let largestWeight = 0; //variable to track the largest weight of a pokemon
   let largestPokemon; //variable to track the largest pokemon
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   //array containing a list of Pokemon
-  let pokemonList = [
-    {
-      name: "Bulbasaur",
-      weight: 6.9,
-      type: ["grass", "poison"],
-    },
-    {
-      name: "Ivysaur",
-      weight: 13,
-      type: ["grass", "poison"],
-    },
-    {
-      name: "Venasaur",
-      weight: 100,
-      type: ["grass", "poison"],
-    },
-  ];
+  let pokemonList = [];
+
+  //defining "loading" elements
+  let heading = document.querySelector('h1')
+  let loadingWrapper = document.createElement('div');
+  let loadingGif = document.createElement('img')
+  let loadingText = document.createElement('p')
+
+  loadingWrapper.classList.add('loading-wrapper')
+  loadingGif.classList.add('loading-gif')
+  loadingGif.src = './img/pikachu-running.gif'
+  loadingText.classList.add('loading-text')
+  loadingText.innerHTML = "Loading..."
+
+  loadingWrapper.appendChild(loadingGif)
+  loadingWrapper.appendChild(loadingText)
+  heading.parentNode.insertBefore(loadingWrapper, heading.nextSibling); //insert loading wrapper after "Pokemon" heading element
+
+  const delay = ms => new Promise(res => setTimeout(res, ms)); //artificial delay function
 
   function add(newPokemon) {
     //base keys to compare
-    let pokeKeys = { name: "", weight: "", type: "" };
+    let pokeKeys = { name: "", detailsUrl: "" };
     //variable that has a function that checks whether two objects have the same keys
     const haveSameKeys = function (obj1, obj2) {
-        return Object.keys(obj1).every((key) => obj2.hasOwnProperty(key));
-      }
+      return Object.keys(obj1).every((key) => obj2.hasOwnProperty(key));
+    };
 
     //check whether new pokemon is an object and has the proper keys
     if (
@@ -48,7 +51,7 @@ let pokemonRepository = (function () {
   function filterPokemon(filterPoke) {
     if (pokemonList.some((poke) => poke.name === filterPoke)) {
       let filteredPoke = pokemonList.filter((poke) => poke.name === filterPoke);
-      displayPokemon(filteredPoke[0]);
+      addListItem(filteredPoke[0]);
     } else {
       console.log("Invalid Pokemon");
     }
@@ -68,13 +71,13 @@ let pokemonRepository = (function () {
   }
 
   //function to add each pokemon as a list item to the pokemon list
-  function displayPokemon(poke) {
+  function addListItem(poke) {
     let pokeItem = document.createElement("li");
-    let button = document.createElement("button")
-    button.innerText = poke.name
-    button.classList.add('poke-button')
-    buttonListener(button, poke)
-    pokeItem.appendChild(button)
+    let button = document.createElement("button");
+    button.innerText = poke.name;
+    button.classList.add("poke-button");
+    buttonListener(button, poke);
+    pokeItem.appendChild(button);
 
     //appending the pokemon to the list
     pokeItem.setAttribute("id", poke.name);
@@ -83,34 +86,88 @@ let pokemonRepository = (function () {
 
   //function to feed into button event listener. logs selected pokemon into the console
   function showDetails(poke) {
-    console.log(poke.name)
+    loadDetails(poke).then(function () {
+      console.log(poke);
+    });
   }
 
   //function to add event listener to button
   function buttonListener(button, poke) {
-    button.addEventListener('click', function() {
-      showDetails(poke)
-    })
+    button.addEventListener("click", function () {
+      showDetails(poke);
+    });
+  }
+
+  async function loadList() {
+    showLoadingMessage();
+    await delay(1000)
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        hideLoadingMessage();
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  async function loadDetails(poke) {
+    showLoadingMessage();
+    await delay(1000)
+    let url = poke.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        hideLoadingMessage();
+        // Now we add the details to the item
+        poke.imageUrl = details.sprites.front_default;
+        poke.height = details.height;
+        poke.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function showLoadingMessage() {
+    loadingWrapper.classList.add('display')
+  }
+
+  function hideLoadingMessage() {
+    loadingWrapper.classList.remove('display')
   }
 
   return {
     add: add,
     getAll: getAll,
-    displayPokemon: displayPokemon,
+    addListItem: addListItem,
     largestPokeNote: largestPokeNote,
     filterPokemon: filterPokemon,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-pokemonRepository.add({
-  name: "Test",
-  weight: 120,
-  type: ["grass", "poison"],
-  test: ""
+pokemonRepository.loadList().then(function () {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
 
-let pokemonList = pokemonRepository.getAll();
+// let pokemonList = pokemonRepository.getAll();
 
-pokemonList.forEach(pokemonRepository.displayPokemon);
+// pokemonList.forEach(pokemonRepository.displayPokemon);
 
 // pokemonRepository.filterPokemon("Bulbasaur");
