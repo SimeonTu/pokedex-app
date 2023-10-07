8c2ce896516c453110752e29cdb0c0b7c8c6de05
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2023-10-07 16:10:31 +0100

    modal UI update

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,23 @@ let pokemonRepository = (function () {
 
   pokeCursors();
 
+  //Function to convert RGB values to HSL. Used later to brighten pokemon thumbnail background color.
+  const RGBToHSL = (r, g, b) => {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const l = Math.max(r, g, b);
+    const s = l - Math.min(r, g, b);
+    const h = s ? (l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s) : 0;
+    let sat = 100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0) + "%";
+    let light = (100 * (2 * l - s)) / 2 + 20;
+    if (light > 100) {
+      light = 100;
+    }
+    light = light + "%";
+    return [60 * h < 0 ? 60 * h + 360 : 60 * h, sat, light].toString();
+  };
+
   const intersectionObserver = new IntersectionObserver(async function (entries) {
     // If intersectionRatio is 0, the target is out of view
     // and we do not need to do anything.
@@ -103,22 +120,6 @@ let pokemonRepository = (function () {
     let img = document.querySelector(`#poke-img-${poke.id}`);
     img.crossOrigin = "Anonymous";
 
-    const RGBToHSL = (r, g, b) => {
-      r /= 255;
-      g /= 255;
-      b /= 255;
-      const l = Math.max(r, g, b);
-      const s = l - Math.min(r, g, b);
-      const h = s ? (l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s) : 0;
-      let sat = 100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0) + "%";
-      let light = (100 * (2 * l - s)) / 2 + 20;
-      if (light > 100) {
-        light = 100;
-      }
-      light = light + "%";
-      return [60 * h < 0 ? 60 * h + 360 : 60 * h, sat, light].toString();
-    };
-
     if (img.complete) {
       let pokeSvg = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" style="opacity:0.75; transform:rotate(145deg);" id="Layer_1" data-name="Layer 1" viewBox="0 0 910.13 908.95"><defs><style>.cls-1{fill:%23fff;opacity:0;}.cls-2{fill:rgb(${colorThief
         .getColor(img)
@@ -127,7 +128,7 @@ let pokemonRepository = (function () {
         .toString()});}</style></defs><title>Pokeball</title><path class="cls-1" d="M337,730c.61-252,204.75-455.12,456.36-454.09,251.82,1,454.28,204.19,453.77,455.38s-204.7,454.42-455.63,453.56C539.59,1184,336.4,980.67,337,730Z" transform="translate(-337 -275.94)"/><path class="cls-2" d="M510,764.63c35.7,0,71.41.21,107.11-.17,7.31-.08,10.2,2,12,9.28,18.82,73.7,86,125.21,163,125.39A168.44,168.44,0,0,0,955.42,773.8c2-7.44,4.77-9.34,12-9.31q107.67.35,215.35,0c8,0,9.52,1.5,8.64,9.91-17.11,163.09-136.13,305.55-302.49,345.19-136.23,32.45-259,1.84-364.57-90.72-72-63.09-113.69-143.59-129.42-237.73-4.43-26.52-4.49-26.5,22.66-26.5Z" transform="translate(-337 -275.94)"/><path class="cls-3" d="M1073.7,695.83c-34.94,0-69.89-.26-104.82.19-8.34.11-11.65-2.73-13.91-10.64C935.18,616,874.39,565.77,806.24,562c-76.78-4.26-141.58,35.28-170.68,104.14-.87,2.07-1.66,4.19-2.38,6.31-8,23.38-8,23.38-33.4,23.38-64.63,0-129.25-.15-193.88.15-9.61,0-13.84-.92-12.11-12.72,22.8-155.69,106.22-266.09,251.65-325,156.73-63.54,330.1-21.39,443.93,103.13,57.07,62.43,90.45,136.3,101,220.32,1.76,14.11,1.64,14.15-12.93,14.16Q1125.56,695.84,1073.7,695.83Z" transform="translate(-337 -275.94)"/><path class="cls-2" d="M793.12,848.53c-64.13.57-118.48-51.88-119-114.84-.55-67.27,51.26-121,117.05-121.43,64.85-.41,118.47,52.27,119.29,117.21C911.3,794,858.11,848,793.12,848.53Z" transform="translate(-337 -275.94)"/><path class="cls-1" d="M793.1,672.52c32.69.37,57.55,26.21,57.15,59.39-.39,31.86-26.4,56.79-59,56.55a57.74,57.74,0,0,1-57.36-58.33C734.31,697.35,760.2,672.15,793.1,672.52Z" transform="translate(-337 -275.94)"/></svg>`;
       document.querySelector(`.poke-item-${poke.id}`).style.backgroundColor = `hsl(${RGBToHSL(...colorThief.getColor(img))})`;
       pokeItem.children(".poke-item").children(".poke-thumbnail").children(".poke-thumbnail-bg").css("background-image", `url('${pokeSvg}')`);
-      console.log("img complete: "+poke.name);
+      console.log("img complete: " + poke.name);
       console.log(RGBToHSL(...colorThief.getColor(img)));
       console.log(colorThief.getColor(img).toString());
     } else {
@@ -139,7 +140,7 @@ let pokemonRepository = (function () {
           .toString()});}</style></defs><title>Pokeball</title><path class="cls-1" d="M337,730c.61-252,204.75-455.12,456.36-454.09,251.82,1,454.28,204.19,453.77,455.38s-204.7,454.42-455.63,453.56C539.59,1184,336.4,980.67,337,730Z" transform="translate(-337 -275.94)"/><path class="cls-2" d="M510,764.63c35.7,0,71.41.21,107.11-.17,7.31-.08,10.2,2,12,9.28,18.82,73.7,86,125.21,163,125.39A168.44,168.44,0,0,0,955.42,773.8c2-7.44,4.77-9.34,12-9.31q107.67.35,215.35,0c8,0,9.52,1.5,8.64,9.91-17.11,163.09-136.13,305.55-302.49,345.19-136.23,32.45-259,1.84-364.57-90.72-72-63.09-113.69-143.59-129.42-237.73-4.43-26.52-4.49-26.5,22.66-26.5Z" transform="translate(-337 -275.94)"/><path class="cls-3" d="M1073.7,695.83c-34.94,0-69.89-.26-104.82.19-8.34.11-11.65-2.73-13.91-10.64C935.18,616,874.39,565.77,806.24,562c-76.78-4.26-141.58,35.28-170.68,104.14-.87,2.07-1.66,4.19-2.38,6.31-8,23.38-8,23.38-33.4,23.38-64.63,0-129.25-.15-193.88.15-9.61,0-13.84-.92-12.11-12.72,22.8-155.69,106.22-266.09,251.65-325,156.73-63.54,330.1-21.39,443.93,103.13,57.07,62.43,90.45,136.3,101,220.32,1.76,14.11,1.64,14.15-12.93,14.16Q1125.56,695.84,1073.7,695.83Z" transform="translate(-337 -275.94)"/><path class="cls-2" d="M793.12,848.53c-64.13.57-118.48-51.88-119-114.84-.55-67.27,51.26-121,117.05-121.43,64.85-.41,118.47,52.27,119.29,117.21C911.3,794,858.11,848,793.12,848.53Z" transform="translate(-337 -275.94)"/><path class="cls-1" d="M793.1,672.52c32.69.37,57.55,26.21,57.15,59.39-.39,31.86-26.4,56.79-59,56.55a57.74,57.74,0,0,1-57.36-58.33C734.31,697.35,760.2,672.15,793.1,672.52Z" transform="translate(-337 -275.94)"/></svg>`;
         document.querySelector(`.poke-item-${poke.id}`).style.backgroundColor = `hsl(${RGBToHSL(...colorThief.getColor(img))})`;
         pokeItem.children(".poke-item").children(".poke-thumbnail").children(".poke-thumbnail-bg").css("background-image", `url('${pokeSvg}')`);
-        console.log("waited for img:"+poke.name);
+        console.log("waited for img:" + poke.name);
         console.log(RGBToHSL(...colorThief.getColor(img)));
         console.log(colorThief.getColor(img).toString());
       });
@@ -367,9 +368,10 @@ let pokemonRepository = (function () {
 
   function filterPokemon(e) {
     $("#gen-dropdown-btn").html("Generation <span id='hamburger'>☰</span>");
-
+    end = 0;
     val = $("input[type='text']").val();
     console.log(val);
+
     if (!val) {
       if ($("#btn-load-pokemon").css("display") == "none") {
         $("#btn-load-pokemon").show();
@@ -536,7 +538,6 @@ let pokemonRepository = (function () {
     modalTitle.empty();
     modalBody.empty();
 
-    // MAKE NEW LOADING WRAPPER!! //
     modalBody.append(loadingWrapper);
     loadingWrapper.addClass("display");
     // await delay(1000);
@@ -569,11 +570,11 @@ let pokemonRepository = (function () {
   function infoItem(poke, property, description) {
     let column = $('<div class="col grid-item"></div');
 
-    let itemTitle = $(`<p class="grid-title"> ${property} </p>`);
     let itemDescription = $(`<p class="grid-description"> ${description} </p>`);
+    let itemTitle = $(`<p class="grid-title"> ${property} </p>`);
 
-    column.append(itemTitle);
     column.append(itemDescription);
+    column.append(itemTitle);
 
     return column;
   }
@@ -582,14 +583,25 @@ let pokemonRepository = (function () {
     let statsColumn = $('<div class="col-12"></div>');
     let statsItem;
 
-    poke.stats.forEach(function (stat) {
+    statsColumn.append($("<p class='grid-title stats'>Base stats</p>"));
+
+    let statTitles = ["HP", "ATK", "DEF", "SP. ATK", "SP. DEF", "SPD"];
+    let statColors = ["red", "orange", "blue", "lightblue", "grey", "green"];
+
+    for (x in poke.stats) {
       statsItem = $('<div class="stats-wrapper"></div>');
-      let statsName = $(`<p class="grid-title"> ${stat.name} </p>`);
+      let statsName = $(`<p class="grid-title"> ${statTitles[x]} </p>`);
       let statsProgress = $(`<div class="progress progress-striped active" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="255">
       <div class="progress-bar progress-bar-success" style="width: 0%"></div>
     </div>`);
-      statsProgress.attr("aria-valuenow", stat.value);
-      statsProgress.children("div").html(stat.value);
+      statsProgress.attr("aria-valuenow", poke.stats[x].value);
+      //Show shorter stat number if progress bar is too small
+      if (poke.stats[x].value / 2.55 < 10) {
+        statsProgress.children("div").html(`${poke.stats[x].value}`);
+      } else {
+        statsProgress.children("div").html(`${poke.stats[x].value}/255`);
+      }
+      statsProgress.children("div").css("background-color", statColors[x]);
       statsItem.append(statsName);
       statsItem.append(statsProgress);
 
@@ -599,11 +611,11 @@ let pokemonRepository = (function () {
         .children("div")
         .children(".progress-bar")
         .animate(
-          { width: stat.value / 2.55 + "%" },
-          1750,
-          "easeOutBounce" //Pokemon stats go from 0 to 255 so in order to get the % width value from 0% to 100% we divide by 2.55
+          { width: poke.stats[x].value / 2.55 + "%" },
+          800,
+          "easeOutCirc" //Pokemon stats go from 0 to 255 so in order to get the % width value from 0% to 100% we divide by 2.55
         );
-    });
+    }
 
     return statsColumn;
   }
@@ -612,37 +624,50 @@ let pokemonRepository = (function () {
     loadDetails(poke).then(function () {
       console.log("loaded details...");
       let modalBody = $(".modal-body");
-      let modalTitle = $(".modal-title");
-      let modalHeader = $(".modal-header");
-      modalTitle.empty();
       modalBody.empty();
 
-      let title = poke.name;
-      let titleSpan = $(`<span> #${poke.id} </span>`);
-
-      let modalInfo = $("<div></div>");
-      modalInfo.addClass("modal-info-wrapper");
-
-      let imageWrapper = $("<div></div>");
-      let image = $("<img />", { src: poke.imageUrl });
-      imageWrapper.addClass("modal-info-image-wrapper");
+      let imageWrapper = $(`<div class='modal-info-image-wrapper modal-info-image-wrapper-${poke.id}'></div>`);
+      let image = $(`<img id="poke-img-modal-${poke.id}" src='${poke.imageUrl}'/>`);
       imageWrapper.append(image);
-      modalInfo.append(imageWrapper);
 
       let infoWrapper = $('<div class="container text-center modal-info-stats-grid"></div>');
+      infoWrapper.css("background", "linear-gradient(356deg, rgba(217,170,255,1) 0%, rgba(255,170,241,1) 100%)");
       let infoRow = $('<div class="row"></div>');
       infoWrapper.append(infoRow);
+
+      //Set Pokemon ID to this format: "#0000"
+      let id = poke.id < 10 ? `#000${poke.id}` : poke.id >= 10 && poke.id < 100 ? `#00${poke.id}` : poke.id >= 100 && poke.id < 1000 ? `#0${poke.id}` : `#${poke.id}`;
+      infoRow.append($(`<p class="modal-poke-name">${poke.name}<span class="modal-poke-id"> ${id}</span></p>`));
+
+      let pokeTypes = $("<div style='display:flex;justify-content:space-around; padding:0;' class='poke-types'></div>");
+      poke.types.forEach(function (type) {
+        pokeTypes.append($(`<span class=" pill bg-color-${type}">${type}</span>`));
+      });
+      infoRow.append(pokeTypes);
+
       infoRow.append(infoItem(poke, "Height", `${poke.height} m`));
       infoRow.append(infoItem(poke, "Weight", `${poke.weight} kg`));
 
       infoRow.append(statsItem(poke));
 
-      modalInfo.append(infoWrapper);
+      imageWrapper.append($('<button type="button" style="position:absolute;right:20px;top:15px" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'));
+      modalBody.append(imageWrapper);
+      modalBody.append(infoWrapper);
 
-      modalTitle.text(title);
-      modalTitle.append(titleSpan);
+      let img = document.querySelector(`#poke-img-modal-${poke.id}`);
+      img.crossOrigin = "Anonymous";
 
-      modalBody.append(modalInfo);
+      if (img.complete) {
+        document.querySelector(`.modal-info-image-wrapper-${poke.id}`).style.backgroundColor = `hsl(${RGBToHSL(...colorThief.getColor(img))})`;
+        console.log("img complete: " + poke.name);
+        console.log(colorThief.getColor(img).toString());
+      } else {
+        img.addEventListener("load", function () {
+          document.querySelector(`.modal-info-image-wrapper-${poke.id}`).style.backgroundColor = `hsl(${RGBToHSL(...colorThief.getColor(img))})`;
+          console.log("img complete: " + poke.name);
+          console.log(colorThief.getColor(img).toString());
+        });
+      }
     });
   }
 
